91e05379fc63f7a783aa7d57d10df999d08b64b5
HANYUJIN <mooo7570@gmail.com>
2024-03-11 16:25:02 +0900

    로딩 및 에러처리

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,14 +1,38 @@
-import Post from './Post';
-import styles from './PostList.module.css';
 import { useQuery } from '@tanstack/react-query';
-import { getPosts } from '../api';
+import { getPosts, getPostsByUsername } from '../api';
+import Post from './Post';
+import { FEED_VARIANT } from '../values';
+import LoadingPage from '../pages/LoadingPage';
+import ErrorPage from '../pages/ErrorPage';
+import styles from './PostList.module.css';
 
-function PostList() {
-  const { data: postsData } = useQuery({
-    queryKey: ['posts'],
-    queryFn: getPosts,
+function PostList({ variant = FEED_VARIANT.HOME_FEED }) {
+  let postsQueryKey;
+  let postsQueryFn;
+
+  if (variant === FEED_VARIANT.HOME_FEED) {
+    postsQueryKey = ['posts'];
+    postsQueryFn = getPosts;
+  } else if (variant === FEED_VARIANT.MY_FEED) {
+    const username = 'codeit';
+    postsQueryKey = ['posts', username];
+    postsQueryFn = () => getPostsByUsername(username);
+  } else {
+    console.warn('Invalid feed request.');
+  }
+
+  const {
+    data: postsData,
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: postsQueryKey,
+    queryFn: postsQueryFn,
   });
-  console.log(postsData);
+
+  if (isPending) return <LoadingPage />;
+
+  if (isError) return <ErrorPage />;
 
   const posts = postsData?.results ?? [];
 
